5c6666638ddc6f341ff995fee62e56dbe07b29fc
KaloyanDanev <kaloian4obg99@gmail.com>
2020-11-10 19:32:25 +0200

    Firebase connection + context

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,20 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { seedDatabase } from '../seed';
+require('dotenv').config({path:__dirname+'/.env'})
+
+
+const config = {
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: "netflix-ce95a.firebaseapp.com",
+    databaseURL: "https://netflix-ce95a.firebaseio.com",
+    projectId: "netflix-ce95a",
+    storageBucket: "netflix-ce95a.appspot.com",
+    messagingSenderId: "131893572895",
+    appId: "1:131893572895:web:2e3d1f33d5bce79e1c2c2a"
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
