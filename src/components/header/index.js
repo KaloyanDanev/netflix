f0e37970a864480708858385857d3a9f332491b6
KaloyanDanev <kaloian4obg99@gmail.com>
2020-11-12 19:31:16 +0200

    Header, Profile, Home page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,21 +1,92 @@
-import React from 'react';
-import { Link as ReactRouterLink } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link as ReactRouterLink} from 'react-router-dom';
 
-import { Background, ButtonLink, Container, Logo } from './styles/header'
+import {
+    Background,
+    ButtonLink,
+    Container,
+    Dropdown,
+    Feature,
+    FeatureCallOut,
+    Group,
+    Link,
+    Logo,
+    Picture,
+    Profile,
+    Search,
+    SearchIcon,
+    SearchInput,
+    Text,
+    PlayButton
+} from './styles/header'
 
 export default function Header({bg = true, children, ...restProps}) {
     return bg ? <Background {...restProps}>{children}</Background> : children
 }
 
-Header.Frame = function HeaderFrame( {children, ...restProps}) {
+Header.Feature = function HeaderFeature({children, ...restProps}) {
+    return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.Profile = function HeaderProfile({children, ...restProps}) {
+    return <Profile {...restProps}>{children}</Profile>
+}
+
+Header.Search = function HeaderSearch({searchTerm, setSearchTerm, ...restProps}) {
+    const [searchActive, setSearchActive] = useState(false);
+
+    return (
+        <Search {...restProps}>
+            <SearchIcon onClick={() => setSearchActive(searchActive => !searchActive)}>
+                <img src="/images/icons/search.png" alt="Search"/>
+            </SearchIcon>
+            <SearchInput
+                value={searchTerm}
+                onChange={({target}) => setSearchTerm(target.value)}
+                placeholder="Search films and series"
+                active={searchActive}
+            />
+        </Search>
+    )
+}
+
+Header.Dropdown = function HeaderDropdown({children, ...restProps}) {
+    return <Dropdown {...restProps}>{children}</Dropdown>
+}
+
+Header.PlayButton = function HeaderPlayButton({children, ...restProps}) {
+    return <PlayButton {...restProps}>{children}</PlayButton>
+}
+
+Header.Text = function HeaderText({children, ...restProps}) {
+    return <Text {...restProps}>{children}</Text>
+}
+
+Header.TextLink = function HeaderTextLink({children, ...restProps}) {
+    return <Link {...restProps}>{children}</Link>
+}
+
+Header.Picture = function HeaderPicture({src, ...restProps}) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`}/>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({children, ...restProps}) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+Header.Frame = function HeaderFrame({children, ...restProps}) {
     return <Container {...restProps}>{children}</Container>
 }
 
-Header.ButtonLink = function HeaderButtonLink( {children, ...restProps}) {
+Header.Group = function HeaderGroup({children, ...restProps}) {
+    return <Group {...restProps}>{children}</Group>
+}
+
+Header.ButtonLink = function HeaderButtonLink({children, ...restProps}) {
     return <ButtonLink {...restProps}>{children}</ButtonLink>
 }
 
-Header.Logo = function HeaderLogo( {to, children, ...restProps}) {
+Header.Logo = function HeaderLogo({to, children, ...restProps}) {
     return (
         <ReactRouterLink to={to}>
             <Logo {...restProps} />
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import {useContent} from "../hooks";
+import {BrowseContainer} from "../containers/browse";
+import selectionFilter from "../utils/selection-map";
 
 export default function Browse() {
- return <p>Browse</p>
+ const {series} = useContent('series');
+ const {films} = useContent('films');
+ const slides = selectionFilter({series,films})
+
+ return <BrowseContainer slides={slides}/>
 };
